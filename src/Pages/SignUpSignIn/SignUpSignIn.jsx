6d17adba75057fe4f57806968ad275a91ce278e7
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-01 14:47:24 +0600

    form

--- src/Pages/SignUpSignIn/SignUpSignIn.jsx
+++ src/Pages/SignUpSignIn/SignUpSignIn.jsx
@@ -3,45 +3,60 @@ import { useState } from "react";
 
 
 const SignUpSignIn = () => {
-    const [value, setValue]=useState({
+    const [post, setPost]=useState({
         email: " ",
         password: " "
     })
 
     const handleCheange = event =>{
-
+       const value=event.target.value
+       
+       setPost({...post, [event.target.name]: value});
     }
 
     const handleSubmit= event =>{
         event.preventDefault();
 
-        axios.post('https://reqres.in/api/login')
-        .then(res => setValue(res))
+        axios.post('https://jsonplaceholder.typicode.com/posts', {post})
+        .then(res => console.log(res.data.post)
+        )
     }
     return (
-        <div>
-            <h1>Login Account</h1>
+        <div className="border-2 w-[40%] mx-auto rounded-lg">
+            <h1 className=" text-2xl font-medium text-center my-4">Sign In Account</h1>
             <form onSubmit={handleSubmit}>
-             <label htmlFor="email" className=" block">
-                Email 
-                <input type="text"
+             <label htmlFor="email" className=" block text-center my-2">
+                Email :
+                <input type="email"
                  placeholder="Enter your email"
                  name="email"
                  id="email"
-                 className=""
+                 className=" ml-10 px-3 py-1 rounded-sm"
+                 onChange={handleCheange}
                  />
                  </label>
 
-             <label htmlFor="password">
-                Password 
+             <label htmlFor="password" className=" block text-center my-2">
+                Password :
                 <input type="password" 
                 placeholder="Enter your password"
                 name="password"
                 id="password"
-                className=""
+                className=" ml-3 px-3 py-1 rounded-sm"
+                onChange={handleCheange}
                 />
              </label>
+            
+            <div className=" text-center my-4">
+            <button type="submit" className="btn">SignIn</button>
+            </div>
+            
+            <hr className=" my-4 w-[80%] mx-auto"/>
 
+            <div className=" text-center my-4">
+                <button className="btn text-purple-500 mr-2">Sign Up</button>
+                <button className="btn text-emerald-500 ml-2">Sign In</button>
+            </div>
             </form>
         </div>
     );
